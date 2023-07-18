0eae34a0f74e4721493cd4d46d3b2639bd130c3c
GtFoBAE05 <y.imannuel@gmail.com>
2023-07-18 09:42:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/petabencana/di/AppModules.kt
@@ -0,0 +1,4 @@
+package com.example.petabencana.di
+
+class AppModules {
+}
---
+++ app/src/main/java/com/example/petabencana/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.petabencana.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petabencana.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
